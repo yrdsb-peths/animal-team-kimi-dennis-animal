62550e5bf0b73ab9dcb8f89027b98b2d991a1e82
Denis126-dot <denishui126@gmail.com>
2026-05-14 19:31:20 -0400

    adding animal monkey, title screen bc and the main game background, sound effect and motion

---
+++ Apple.java
@@ -0,0 +1,25 @@
+import greenfoot.*;
+
+public class Apple extends Actor
+{
+    int speed = 1;
+    public void act()
+    {
+        int x = getX();
+        int y = getY() + speed;
+        setLocation(x, y);
+        
+        
+        MyWorld world = (MyWorld) getWorld();
+        if(getY() >= world.getHeight())
+        {
+            world.gameOver();
+            world.removeObject(this);
+        }
+    }
+    
+    public void setSpeed(int spd)
+    {
+        speed=spd;
+    }
+}
--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,46 @@
 import greenfoot.*;
 
-public class MyWorld extends World {
-    public MyWorld() {
-        super(600, 400, 1);
+public class MyWorld extends World 
+{
+    public int score = 0;
+    Label scoreLabel;
+    int level = 1;
+    public MyWorld() 
+    {
+        super(600, 400, 1, false);
+
+        Monkey monkey = new Monkey();
+        addObject(monkey, 200, 200);
+
+        scoreLabel = new Label("0", 100);
+        addObject(scoreLabel, 50, 50);
+
+        createApple();
+    }
+
+    public void gameOver()
+    {
+        Label gameOverLabel = new Label("😂Game Over😂", 75);
+        addObject(gameOverLabel, 300, 200);
+    }
+
+    public void increaseScore()
+    {
+        score++;
+        scoreLabel.setValue(score);
+        
+        if(score % 5 == 0)
+        {
+            level++;
+        }
+    }
+
+    public void createApple()
+    {
+        Apple apple = new Apple();
+        apple.setSpeed(level);
+        int x = Greenfoot.getRandomNumber(600);
+        int y = 0;
+        addObject(apple, x, y);
     }
 }
